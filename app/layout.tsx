ceb2ed866ffaf2bebdc5c12ab41167287b806aca
heeseopark <heeseopark@korea.ac.kr>
2024-10-28 13:17:26 +0900

    feat: pretendard font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,34 +2,36 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900"
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900"
+// });
+
+const pretendard = localFont({
+  src: "./fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920"
 });
 
 export const metadata: Metadata = {
   title: "사랑의교회 대학부 수양회 신청",
-  description: "사랑의교회 대학부 수양회 신청 페이지입니다.",
+  description: "사랑의교회 대학부 수양회 신청 페이지입니다."
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${pretendard.className} antialiased`}>{children}</body>
     </html>
   );
 }
