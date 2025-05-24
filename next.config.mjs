209a597558922c4215ae69518df981613b6e0210
heeseopark <heeseopark@korea.ac.kr>
2025-05-24 18:02:59 +0900

    feat: additional setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NEXT_PUBLIC_SERVER_URL:
-      process.env.NEXT_PUBLIC_SERVER_URL || "https://dev.api.sarang-univ.com"
-  },
   images: {
     remotePatterns: [
       {
@@ -14,16 +10,6 @@ const nextConfig = {
       }
     ]
   },
-  async rewrites() {
-    const serverUrl =
-      process.env.NEXT_PUBLIC_SERVER_URL || "https://dev.api.sarang-univ.com";
-    return [
-      {
-        source: "/api/:path*",
-        destination: serverUrl + "/api/:path*"
-      }
-    ];
-  }
 };
 
 export default nextConfig;
